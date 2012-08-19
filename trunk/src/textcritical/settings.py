b906d21c8e4b8acf604325c60158435d41567169
Luke <Luke@2898fa77-1178-43f1-a05b-82af898f5153>
2012-08-19 06:22:06 +0000

    Committing initial Django project

---
+++ trunk/src/textcritical/settings.py
@@ -0,0 +1,11 @@
+from textcritical.default_settings import *
+
+MEDIA_URL = 'http://127.0.0.1:8080/static'
+
+MEDIA_ROOT = 'C:/Users/Luke/Workspace/TextCritical.com/src/media/'
+
+STATIC_URL = 'http://127.0.0.1:8080/static/'
+
+STATICFILES_DIRS = (
+    'C:/Users/Luke/Workspace/TextCritical.com/src/media/'
+)
