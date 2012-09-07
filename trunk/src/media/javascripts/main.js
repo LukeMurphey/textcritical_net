dfe2abcc53f34e69cb594dcd2eb356f8c41da260
Luke <Luke@2898fa77-1178-43f1-a05b-82af898f5153>
2012-09-07 04:30:45 +0000

    Using require.js to load the javascript files; added jqueryUI and the datatables plugin

---
+++ trunk/src/media/javascripts/main.js
@@ -0,0 +1,22 @@
+require.config({
+    baseUrl: "media/javascripts",
+    paths: {
+        jquery: [
+            "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery",
+            "libs/jquery-1.7.1"
+        ],
+        jqueryui: [
+            "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui",
+            "libs/jqueryui"
+        ],
+             
+        datatables: [
+            "http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.3/jquery.dataTables",
+            "libs/jquery.dataTables"
+        ]
+    },
+    shim: {
+        'jqueryui': ['jquery'],
+        'datatables': ['jquery']
+    }
+});
