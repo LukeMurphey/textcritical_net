716573fafedf08b6c4b349815d3167599e1cd3c8
Luke <Luke@2898fa77-1178-43f1-a05b-82af898f5153>
2012-09-11 03:44:10 +0000

    Adding in Grappelli

--- trunk/src/textcritical/settings.py
+++ trunk/src/textcritical/settings.py
@@ -1,11 +1,5 @@
 from textcritical.default_settings import *
 
-MEDIA_URL = 'http://127.0.0.1:8080/media'
+MEDIA_URL = '/media'
 
 MEDIA_ROOT = 'C:/Users/Luke/Workspace/TextCritical.com/src/media/'
-
-STATIC_URL = 'http://127.0.0.1:8080/static/'
-
-STATICFILES_DIRS = (
-    'C:/Users/Luke/Workspace/TextCritical.com/src/media/'
-)
