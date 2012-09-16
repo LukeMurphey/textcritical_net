872f78fbe5694422fedb17d716d705c61e8992b0
Luke <Luke@2898fa77-1178-43f1-a05b-82af898f5153>
2012-09-16 19:31:05 +0000

    Added logging to the default settings file

--- trunk/src/textcritical/settings.py
+++ trunk/src/textcritical/settings.py
@@ -3,3 +3,61 @@ from textcritical.default_settings import *
 MEDIA_URL = '/media'
 
 MEDIA_ROOT = 'C:/Users/Luke/Workspace/TextCritical.com/src/media/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler'
+        },
+        'default': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': '../var/log/app.log',
+            'maxBytes': 1024*1024*5, # 5 MB
+            'backupCount': 5,
+            'formatter':'standard',
+        },
+        'db': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': '../var/log/db.log',
+            'maxBytes': 1024*1024*5, # 5 MB
+            'backupCount': 5,
+            'formatter':'standard',
+        },
+    },
+    'loggers': {
+        'django.db': {
+            'handlers': ['db'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['default'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
