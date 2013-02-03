d842910cbb25f18e82957239edaf1cbd20941ed8
Luke <Luke@2898fa77-1178-43f1-a05b-82af898f5153>
2013-02-03 20:27:31 +0000

    Added command for rebuilding the search indexes

---
+++ trunk/src/reader/management/commands/make_search_indexes.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand
+
+from reader.contentsearch import WorkIndexer
+from reader.models import Work
+from django.db.models import Q
+
+from optparse import make_option
+
+class Command(BaseCommand):
+
+    help = "Creates the indexes necessary for facilitating searches"
+
+    option_list = BaseCommand.option_list + (
+        make_option("-w", "--work", dest="work", help="The work to index"),
+        make_option("-c", "--clear", action="store_true", dest="clear_indexes", default=False, help="Clear the existing indexes before starting")
+    )
+
+    def handle(self, *args, **options):
+        
+        work_title  = options['work']
+        
+        if work_title is None and len(args) > 0:
+            work_title = args[0]
+            
+        
+        clear_indexes  = options['clear_indexes']
+        
+        # Make the directory if it does not exist
+        if clear_indexes or not WorkIndexer.index_dir_exists():
+            WorkIndexer.get_index(create=True)
+        
+        # Index all works if no work was provided
+        if work_title is None:
+            print "Creating search indexes..."
+            WorkIndexer.index_all_works()
+            print "Search indexes successfully created"
+            
+        # Index the provided work
+        else:
+            
+            # Try to find the work
+            try:
+                work = Work.objects.get( Q(title=work_title) | Q(title_slug=work_title) )
+                           
+                print "Creating search indexes for work..."
+                WorkIndexer.index_work(work)
+                print "Search indexes successfully created"
+            except Work.DoesNotExist:
+                print "Work could not be found with the given title"
