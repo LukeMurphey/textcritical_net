7a082dc2615e39557f643db4e045f06936320193
Luke <Luke@2898fa77-1178-43f1-a05b-82af898f5153>
2013-03-07 06:41:50 +0000

    Added template tag that allows morphological tagging to work on the LXX

--- src/reader/templatetags/reader_extras.py
+++ src/reader/templatetags/reader_extras.py
@@ -1,6 +1,7 @@
 import re
 from django import template
 from reader.shortcuts import convert_xml_to_html5, convert_xml_to_html5_minidom
+from xml.dom import minidom
 from reader.language_tools import transform_text
 import random
 
@@ -24,6 +25,52 @@ def xml_to_html5(value, language=None):
     finally:
         converted_doc.unlink()
         del(converted_doc)
+
+@register.filter(name='unbound_text_to_html5')
+def unbound_text_to_html5(text, language=None):
+    """
+    Converts the provided text to HTML5 custom data attributes.
+    
+    Usage:
+    {{text|unbound_text_to_html5:"Greek"}}
+    """
+    
+    # Make the document that will contain the verse
+    converted_doc = minidom.Document()
+    
+    # Make the verse node to attach the content to
+    verse_node = converted_doc.createElement( "span" )
+    verse_node.setAttribute("class", "verse")
+    
+    # Append the 
+    converted_doc.appendChild(verse_node)
+    
+    # Split up the text and place the text segments in nodes
+    segments = re.findall("[\s]+|[\[\],.:.;]|[^\s\[\],.:.;]+", text)
+    
+    for s in segments:
+        
+        # Don't wrap punctuation in a word node
+        if s in [";", ",", ".", "[", "]", ":"] or len(s.strip()) == 0:
+            txt_node = converted_doc.createTextNode( s )
+            verse_node.appendChild( txt_node )
+        
+        else:
+            word_node = converted_doc.createElement( "span" )
+            word_node.setAttribute( "class", "word" )
+            
+            # Create the text node and append it
+            if language is None:
+                txt_node = converted_doc.createTextNode( s )
+            else:
+                txt_node = converted_doc.createTextNode( transform_text(s, language).decode( "utf-8" ) )
+            
+            word_node.appendChild(txt_node)
+           
+            # Append the node
+            verse_node.appendChild(word_node)
+    
+    return converted_doc.toxml( encoding="utf-8" )
 
 @register.filter(name='perseus_xml_to_html5')
 def perseus_xml_to_html5(value, language=None):
