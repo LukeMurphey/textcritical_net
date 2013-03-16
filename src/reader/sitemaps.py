95944f03e71448b680ca0f1d2cb2e83f6d28b14e
Luke <Luke@2898fa77-1178-43f1-a05b-82af898f5153>
2013-03-16 09:04:07 +0000

    Added a sitemap. Closes #580.

---
+++ src/reader/sitemaps.py
@@ -0,0 +1,34 @@
+from django.contrib.sitemaps import Sitemap
+from reader.models import Work
+from django.core.urlresolvers import reverse
+
+class StaticSitemap(Sitemap):
+    priority = 0.5
+    lastmod = None
+ 
+    def items(self):
+        return [
+                "/",
+                reverse('works_index'),
+                reverse('about'),
+                reverse('contact'),
+                reverse('search')
+            ]
+ 
+    def location(self, obj):
+        return obj[0] if isinstance(obj, tuple) else obj
+ 
+    def changefreq(self, obj):
+        return obj[1] if isinstance(obj, tuple) else "monthly"
+
+class WorksSitemap(Sitemap):
+    changefreq = "never"
+    priority = 0.5
+
+    def items(self):
+        return Work.objects.all()
+
+    def location(self, work):
+        return reverse('read_work', kwargs={'title': work.title_slug})
+    
+    
