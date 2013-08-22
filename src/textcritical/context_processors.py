6f4428368448a8824c6f91fb62b4fc2d9664beca
Luke <Luke@2898fa77-1178-43f1-a05b-82af898f5153>
2013-08-22 17:39:21 +0000

    Added support for Google Analytics

--- src/textcritical/context_processors.py
+++ src/textcritical/context_processors.py
@@ -32,6 +32,7 @@ def global_settings(request):
     
     settings_dict['USE_DARK_THEME'] = get_setting_or_default("USE_DARK_THEME", False)
     settings_dict['USE_MINIFIED'] = get_setting_or_default("USE_MINIFIED", False)
+    settings_dict['GOOGLE_ANALYTICS_ID'] = get_setting_or_default("GOOGLE_ANALYTICS_ID", False)
     
     return settings_dict
 
