dbd302fb88d2dfc8afa46c52f562c496cff9d858
Luke <Luke@2898fa77-1178-43f1-a05b-82af898f5153>
2013-09-12 16:39:32 +0000

    Switched out the make_epub command with a version that makes multiple epub documents

---
+++ src/reader/management/commands/make_epubs.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+from django.conf import settings
+
+from reader.models import Work
+from reader.ebook import ePubExport
+
+import sys
+import os
+from optparse import make_option
+import traceback
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+
+    help = "Produces an ePub from a work"
+
+    option_list = BaseCommand.option_list + (
+        make_option("-f", "--force", action="store_true", default=False, dest="force", help="Force creation of new works even if they already exist"),
+    )
+
+    def handle(self, *args, **options):
+        
+        force  = options['force']
+        
+        # Try to find the work
+        try:
+            works = Work.objects.all()
+            
+            for work in works:
+                
+                epub_file = work.title_slug + ".epub"
+                epub_file_full_path = os.path.join( settings.GENERATED_FILES_DIR, epub_file)
+                
+                # Make the epub unless it already exist or if we are forcing it
+                if force or not os.path.exists(epub_file_full_path):
+                    fname = ePubExport.exportWork(work, epub_file_full_path)
+                    logger.info("Created epub in, filename=%s", fname)
+                    print "Created epub in, filename=", fname
+            
+        except Exception as e:
+            traceback.print_exc()
