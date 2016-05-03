dd393e466555f10e0f5d8c5bc8f8a90736a0b075
Luke <Luke@2898fa77-1178-43f1-a05b-82af898f5153>
2016-05-03 06:51:37 +0000

    Adding ability to lookup multiple topics

--- trunk/src/reader/urls.py
+++ trunk/src/reader/urls.py
@@ -54,5 +54,6 @@ urlpatterns = patterns('',
     url(r'^api/works_typeahead_hints/?$', 'reader.views.api_works_typeahead_hints', name='api_works_typeahead_hints' ),
     
     url(r'^api/wikipedia_info/(?P<topic>[^/]*)/?$', 'reader.views.api_wikipedia_info', name='api_wikipedia_info' ),
+    url(r'^api/wikipedia_info/(?P<topic>[^/]*)/(?P<topic2>[^/]*)/?$', 'reader.views.api_wikipedia_info', name='api_wikipedia_info_2' ),
     
 )
