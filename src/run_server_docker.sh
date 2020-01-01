e82ccdfb77974ff17fe368bfb06be38f62a3a64c
Luke Murphey <Luke.Murphey@gmail.com>
2019-12-31 20:39:33 -0800

    Adding loading of resources into the media directory on startup

--- src/run_server_docker.sh
+++ src/run_server_docker.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 cd /usr/src/app
 mkdir -p /usr/src/app/var/log/
+
+deploy_static() {
+    # Ensure that the static directory was declared
+    if [ -d "/media" ]
+    then
+        # Copy over the files if it was
+        cp -r /usr/src/app/media/* /media
+    fi
+}
+
+deploy_static
 python run_server.py
