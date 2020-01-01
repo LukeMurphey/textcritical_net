98d2e74303663e23cc86ccc84cdd715754a0e14a
Luke Murphey <Luke.Murphey@gmail.com>
2019-12-31 22:09:37 -0800

    Moving the static files under the /media directory

--- src/run_server_docker.sh
+++ src/run_server_docker.sh
@@ -2,18 +2,20 @@
 cd /usr/src/app
 mkdir -p /usr/src/app/var/log/
 
+$STATIC_DIR=/static
+
 deploy_static() {
     # Ensure that the static directory was declared
-    if [ -d "/media" ]
+    if [ -d $STATIC_DIR ]
     then
         # Copy over the files if it was
-        cp -r /usr/src/app/media/font /media
-        cp -r /usr/src/app/media/images /media
-        cp -r /usr/src/app/media/javascripts /media
-        cp -r /usr/src/app/media/static /media
-        cp -r /usr/src/app/media/stylesheets /media
-        cp -r /usr/src/app/media/templates /media
-        cp /usr/src/app/reader/templates/503.html /media
+        cp -r /usr/src/app/media/font $STATIC_DIR/media
+        cp -r /usr/src/app/media/images $STATIC_DIR/media
+        cp -r /usr/src/app/media/javascripts $STATIC_DIR/media
+        cp -r /usr/src/app/media/static $STATIC_DIR/media
+        cp -r /usr/src/app/media/stylesheets $STATIC_DIR/media
+        cp -r /usr/src/app/media/templates $STATIC_DIR/media
+        cp /usr/src/app/reader/templates/503.html $STATIC_DIR
     fi
 }
 
