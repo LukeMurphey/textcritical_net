bb6a8ed8cb5169a29c23d1a359a96f1d918b8be9
Luke Murphey <Luke.Murphey@gmail.com>
2020-01-14 11:30:38 -0800

    Moving wsgi server out from the app itself

--- src/textcritical/server.py
+++ src/textcritical/server.py
@@ -1,7 +1,7 @@
 from django.core.wsgi import get_wsgi_application
+import wsgiserver
 
 def start_server( address="0.0.0.0", port=8080):
-    from textcritical import wsgiserver
     
     server = wsgiserver.CherryPyWSGIServer(
         (address, port),  # Use '127.0.0.1' to only bind to the localhost
