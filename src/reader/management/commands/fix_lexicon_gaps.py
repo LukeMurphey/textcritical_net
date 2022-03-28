674e839750c312fb8dc48fa004aac51d35a7bb1c
Luke Murphey <Luke.Murphey@gmail.com>
2022-03-27 19:54:01 -0700

    Adding missing lemmas

--- src/reader/management/commands/fix_lexicon_gaps.py
+++ src/reader/management/commands/fix_lexicon_gaps.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 
 from reader.models import WordDescription, Lemma, LexiconEntry, WordForm
+from reader.utils import get_lemma
 
 class Command(BaseCommand):
 
@@ -15,3 +16,18 @@ class Command(BaseCommand):
 
         # Found
         print("Found", len(lexicon_entries_gaps), "lexicon entries with missing lemmas")
+
+        entries_updated = 0
+    
+        for lexicon_entry in lexicon_entries_gaps:
+            # Find a matching lemma
+            lemma = get_lemma(lexicon_entry.form)
+
+            # Assign the lemma
+            if lemma is not None:
+                lexicon_entry.lemma = lemma
+                lexicon_entry.save()
+
+                entries_updated = entries_updated + 1
+
+        print("Updated", entries_updated, "lexicon entries")
