0ba2a147f85376a9f7d42ca27a475bb7b0be6dbe
Luke Murphey <Luke.Murphey@gmail.com>
2022-04-01 10:15:41 -0700

    Simplifying the run script

--- src/run_server_docker.sh
+++ src/run_server_docker.sh
@@ -8,13 +8,9 @@ deploy_static() {
     # Ensure that the static directory was declared
     if [ -d $STATIC_DIR ]
     then
-        # Copy over the files if it was
-        cp -r /usr/src/app/media/font $STATIC_DIR/media
-        cp -r /usr/src/app/media/images $STATIC_DIR/media
-        cp -r /usr/src/app/media/javascripts $STATIC_DIR/media
-        cp -r /usr/src/app/media/static $STATIC_DIR/media
-        cp -r /usr/src/app/media/stylesheets $STATIC_DIR/media
-        cp -r /usr/src/app/media/templates $STATIC_DIR/media
+        # Copy over the files
+        mkdir -p $STATIC_DIR/media
+        cp -r /usr/src/app/media/* $STATIC_DIR/media
         cp /usr/src/app/reader/templates/503.html $STATIC_DIR/
         echo "Done copying files to static"
     fi
