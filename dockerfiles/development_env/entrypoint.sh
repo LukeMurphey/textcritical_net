68de5d566dd44555f686eb79aa7cab38f9751201
Luke Murphey <Luke@LukeMurphey.net>
2023-04-20 00:21:02 -0400

    Adding comments to the dev entrypoint

--- dockerfiles/development_env/entrypoint.sh
+++ dockerfiles/development_env/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cd /git/textcritical_spa
 
+# Run yarn install in case something has changed
 yarn install
+
 # We are using 'start_listen_all' need to have webpack not restrict itself to local host because
 # inbound network connections are coming through Docker's bridge which will appear to be an external
 # host
