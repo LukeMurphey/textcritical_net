ed583009e72c6618a2cc6b70c9ee8659ddb709bf
Luke Murphey <Luke@LukeMurphey.net>
2023-04-20 19:20:49 -0400

    Improving development environment

--- dockerfiles/development_env/entrypoint.sh
+++ dockerfiles/development_env/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 cd /git/textcritical_spa
 
-# Run yarn install in case something has changed
-yarn install
+while true
+do
+    # Run yarn install in case something has changed
+    yarn install
 
-# We are using 'start_listen_all' need to have webpack not restrict itself to local host because
-# inbound network connections are coming through Docker's bridge which will appear to be an external
-# host
-yarn run start_listen_all
+    # We are using 'start_listen_all' need to have webpack not restrict itself to local host because
+    # inbound network connections are coming through Docker's bridge which will appear to be an external
+    # host
+    yarn run start_listen_all
+
+    # Sleep for a bit so that a problem doesn't spike the CPU usage
+    sleep 3
+done
