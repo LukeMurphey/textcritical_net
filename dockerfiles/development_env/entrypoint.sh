716581510f86efe74a1649e1c10b8a1f538551d0
Luke Murphey <Luke@LukeMurphey.net>
2023-04-19 22:01:55 -0400

    Adding development environment container

---
+++ dockerfiles/development_env/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd /git/textcritical_spa
+
+yarn install
+# We are using 'start_listen_all' need to have webpack not restrict itself to local host because
+# inbound network connections are coming through Docker's bridge which will appear to be an external
+# host
+yarn run start_listen_all
