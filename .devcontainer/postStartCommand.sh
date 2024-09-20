4c3a41a058c535250110ce5cd513e9922fa5a7aa
LukeMurphey <luke.murphey@gmail.com>
2024-09-20 02:05:01 +0000

    More fixes to the dev container

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -3,11 +3,13 @@
 git submodule update --init
 cd /workspace/submodules/textcritical_spa
 
-yarn install
-yarn run start_listen_all
+pip install --no-cache-dir -r /workspace/src/requirements.txt
 
 # Copy over the settings file
-cp /workspace/.devcontainer/settings.py /workspace/src/textcritical/
+#cp /workspace/.devcontainer/settings.py /workspace/src/textcritical/
 
 # Copy the properties file
-cp /workspace/dockerfiles/development_env/dev.local.properties /workspace/local.properties
+#cp /workspace/dockerfiles/development_env/dev.local.properties /workspace/local.properties
+
+yarn install
+yarn run start_listen_all
